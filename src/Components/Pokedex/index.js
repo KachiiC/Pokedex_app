b50bbf536d3e2e5217d65878059f98830a87b4e8
Kachii <Kacheokcheong@gmail.com>
2020-10-31 22:13:17 +0000

    added working pokedex

--- src/Components/Pokedex/index.js
+++ src/Components/Pokedex/index.js
@@ -1,11 +1,17 @@
-import React, { cloneElement, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useForm} from 'react-hook-form'
 
 const Pokedex = () => {
 
     const {register, handleSubmit} = useForm();
     const [pokeCode, setPokeCode] = useState("")
-    const [pokeData, setPokeData] = useState()
+    const [pokeImage, setPokeImage] = useState("")
+    const [pokeData, setPokeData] = useState(
+        {
+            "name": "",
+            "id": ""
+        }
+    )
 
     const onSubmit = data => {
         const submittedCode = document.getElementById("inputName").value
@@ -15,7 +21,10 @@ const Pokedex = () => {
     useEffect(() => {
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokeCode}`)
         .then(response => response.json())
-        .then(responseData => setPokeData(responseData))
+        .then(responseData => {
+            setPokeData(responseData)
+            setPokeImage(responseData.sprites.front_default)
+        })
         .catch(err => console.log(err))
 
     },[pokeCode])
@@ -26,8 +35,13 @@ const Pokedex = () => {
         <>
             <h1> Pokedex Form </h1>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input id="inputName" name="pokemonName" ref={register} placeholder="Enter Pokemon" />
+                <input id="inputName" name="pokemonName" ref={register} placeholder="Enter Pokemon name or nubmer" />
             </form>
+            <div>
+                <img src={pokeImage} alt="pokedex-pokemon"/>
+                <h1>{pokeData.name}</h1>
+                <h6>Pokemon #: {pokeData.id}</h6>
+            </div>
         </>
     )
 }
